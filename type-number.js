076fc42b683aadd35f43797915878b8bdecee624
= <eduardoscespes@gmail.com>
2021-11-08 18:24:09 -0300

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,40 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+
+console.log(operacaoMatematica);
+
+
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const numeroPontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+
+// NaN -> Not a Number (não é um número)
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero);
+
+
+var a = 0;
+var b = 10;
+console.log(a/b);
+
+
+
+
+
+
---
+++ type-string.js
@@ -0,0 +1,18 @@
+
+const texto1 = "Ola, Mundo!";
+const texto2 = 'Olá Mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+
+//template String ou tamplete literal
+
+
