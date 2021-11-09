33300e7783c144c4f50e5b61b1c09c6de9265f88
= <eduardoscespes@gmail.com>
2021-11-09 16:12:08 -0300

    codigos var e conversoes

---
+++ truthy-falsy.js
@@ -0,0 +1,15 @@
+//boolean
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy ou Falsy
+ // 0 => false;
+ // 1 => true;
+
+ console.log(0 == false)
+ console.log(1 == false)
+ console.log (1 == true)
+
+ // null
+ // underfined
+
